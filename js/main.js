74a98ba6474bc0fc83d488ab8efe1f41c506136d
MDrakos <mike.drakos@gmail.com>
2017-06-18 21:50:40 -0700

    complete enough for submission

--- js/main.js
+++ js/main.js
@@ -3,17 +3,16 @@
  */
 
 $(document).ready(function(){
-    var arrResults = [];
+    var results = [];
     var html = '';
 
-// Create structure for the data
+    // Store results as objects
     function Result(title, snippet) {
         this.title = title;
         this.snippet = snippet;
     }
 
     function search() {
-        // Use Ajax to handle things
         $.ajax({
             url: 'https://en.wikipedia.org/w/api.php?action=query&list=search&format=json&srsearch=' + $('#search').val(),
             dataType: 'jsonp',
@@ -23,41 +22,28 @@ $(document).ready(function(){
             },
             success: function(data) {
 
-                // First we clear the children from our class to make sure no previous results are showing.
+                // Clear results, in the case of another search
                 $('.results').empty();
+                results.length = 0;
 
-                // Then we also clear the array with the results before providing new information.
-                arrResults.length = 0;
-                var resArr = data.query.search;
+                // store received data
+                var received = data.query.search;
 
-                //For each result, generate the html data.
-                for (var result in resArr) {
-                    arrResults.push(new Result(resArr[result].title, resArr[result].snippet));
-                    html = '<div id="articles" class="well"><a href="https://en.wikipedia.org/wiki/' + resArr[result].title + '"target="_blank"><h3>' + resArr[result].title + '</h3><p>' + resArr[result].snippet + '</p></a></div>';
-
-                    // Displays the elements to the page
+                // generate each article results
+                for (var result in received) {
+                    results.push(new Result(received[result].title, received[result].snippet));
+                    html = '<div id="articles" class="well"><a href="https://en.wikipedia.org/wiki/' + received[result].title + '"target="_blank"><h3>' + received[result].title + '</h3><p>' + received[result].snippet + '</p></a></div>';
                     $('.results').append(html);
                 }
             }
         });
-
-        // This will handle when to display results based on the search bar.
-        if ($('#search').val().length > 0) {
-            $('.articles').css('display', 'none');
-
-        } else if ($('#search').val().length < 1) {
-            // display everything again
-            $('.articles').css('display', 'block');
-        }
-
-        // This make things tick with each key stroke
-        $('#search').unbind('keyup');
-        $('#search').keyup(function() {
-            search();
-        });
     }
 
-    $('#search').keyup(function() {
-        search();
+    $('#search').keyup(function (e) {
+        if(e.which === 13) {
+            search();
+        } else if (!$(this).val()) {
+            $('.results').empty();
+        }
     });
 });
